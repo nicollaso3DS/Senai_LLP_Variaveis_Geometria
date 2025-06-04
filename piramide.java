1ea6b257dd656584a1f42e6f1e5636e7c9a96df6
nicollaso3DS <nicollasoliveira707@gmail.com>
2025-06-04 20:17:36 -0300

    Update piramide.java

--- piramide.java
+++ piramide.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
 public class piramide {
-    
+    public class Piramide {
+    public static double calcularArea(double ladoBase, double alturaFaceLateral) {
+        double areaBase = ladoBase * ladoBase;
+        double areaLateral = 2 * ladoBase * alturaFaceLateral;
+        return areaBase + areaLateral;
+    }
+
+    public static void main(String[] args) {
+        double area = calcularArea(4, 6);
+        System.out.println("Área da Pirâmide: " + area);
+    }
+}
+
 }
