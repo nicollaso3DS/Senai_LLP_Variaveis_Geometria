f461d95088d1a43ee618aa60cc1cafa54e2fe143
nicollaso3DS <nicollasoliveira707@gmail.com>
2025-06-04 20:19:38 -0300

    Update bola.java

--- bola.java
+++ bola.java
@@ -1,3 +1,13 @@
 public class bola {
-    
+    public class Bola {
+    public static double calcularArea(double raio) {
+        return 4 * Math.PI * raio * raio;
+    }
+
+    public static void main(String[] args) {
+        double area = calcularArea(6);
+        System.out.println("Área da Bola: " + area);
+    }
+}
+
 }
