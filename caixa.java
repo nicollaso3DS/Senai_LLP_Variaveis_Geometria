207a971b40425b2456773e3b8567cfb504d1c6c5
nicollaso3DS <nicollasoliveira707@gmail.com>
2025-06-04 20:19:58 -0300

    Update caixa.java

--- caixa.java
+++ caixa.java
@@ -1,3 +1,13 @@
 public class caixa {
-    
+    public class Caixa {
+    public static double calcularArea(double largura, double altura, double profundidade) {
+        return 2 * (largura * altura + largura * profundidade + altura * profundidade);
+    }
+
+    public static void main(String[] args) {
+        double area = calcularArea(5, 10, 3);
+        System.out.println("Área da Caixa: " + area);
+    }
+}
+
 }
