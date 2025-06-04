a0f16aaf62afd862cb807608f5a07dfed6ed6cce
nicollaso3DS <nicollasoliveira707@gmail.com>
2025-06-04 20:58:37 -0300

    Update tubo.java

--- tubo.java
+++ tubo.java
@@ -1,3 +1,15 @@
 public class tubo {
-    
+    public class Tubo {
+    public static double calcularArea(double raio, double altura) {
+        double areaLateral = 2 * Math.PI * raio * altura;
+        double areaBases = 2 * Math.PI * raio * raio;
+        return areaLateral + areaBases;
+    }
+
+    public static void main(String[] args) {
+        double area = calcularArea(3, 8);
+        System.out.println("Área do Tubo: " + area);
+    }
+}
+
 }
